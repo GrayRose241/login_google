f9d473dfb71529c4c1eb05e7259223a77cea372c
GrayRose241 <103165408+GrayRose241@users.noreply.github.com>
2022-11-23 23:54:53 +0700

    - add apple lib

--- Assets/Network/GooglePlayGameControl.cs
+++ Assets/Network/GooglePlayGameControl.cs
@@ -1,5 +1,18 @@
-﻿using GooglePlayGames;
+﻿#if UNITY_ANDROID
+using GooglePlayGames;
 using GooglePlayGames.BasicApi;
+#endif
+
+#if UNITY_IOS
+using AppleAuth;
+using AppleAuth.Enums;
+using AppleAuth.Extensions;
+using AppleAuth.Interfaces;
+using AppleAuth.Native;
+#endif
+
+
+using System.Text;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
@@ -7,13 +20,32 @@ public class GooglePlayGameControl : MonoBehaviour
 {
     private string Token;
 
+#if UNITY_IOS
+    private AppleAuthManager appleAuthManager;
+
+
+#endif
+
     void Awake()
     {
+#if UNITY_ANDROID
         PlayGamesPlatform.Activate();
+#endif
+
+#if UNITY_IOS
+        if (AppleAuthManager.IsCurrentPlatformSupported)
+        {
+            // Creates a default JSON deserializer, to transform JSON Native responses to C# instances
+            var deserializer = new PayloadDeserializer();
+            // Creates an Apple Authentication manager with the deserializer
+            this.appleAuthManager = new AppleAuthManager(deserializer);
+        }
+#endif
     }
 
     public void LoginGooglePlayGames()
     {
+#if UNITY_ANDROID
         PlayGamesPlatform.Instance.Authenticate((success) =>
         {
             if (success == SignInStatus.Success)
@@ -33,11 +65,55 @@ public class GooglePlayGameControl : MonoBehaviour
                 Debug.Log("Login Unsuccessful");
             }
         });
+#endif
+
+#if UNITY_IOS
+        var loginArgs = new AppleAuthLoginArgs(LoginOptions.IncludeEmail | LoginOptions.IncludeFullName);
+
+        this.appleAuthManager.LoginWithAppleId(loginArgs, credential =>
+            {
+                // Obtained credential, cast it to IAppleIDCredential
+                var appleIdCredential = credential as IAppleIDCredential;
+                if (appleIdCredential != null)
+                {
+                    // Apple User ID
+                    // You should save the user ID somewhere in the device
+                    var userId = appleIdCredential.User;
+                    //PlayerPrefs.SetString(AppleUserIdKey, userId);
+
+                    // Email (Received ONLY in the first login)
+                    var email = appleIdCredential.Email;
+
+                    // Full name (Received ONLY in the first login)
+                    var fullName = appleIdCredential.FullName;
+
+                    // Identity token
+                    Token = Encoding.UTF8.GetString(appleIdCredential.IdentityToken, 0, appleIdCredential.IdentityToken.Length);
+
+                    // Authorization code
+                    var authorizationCode = Encoding.UTF8.GetString(appleIdCredential.AuthorizationCode, 0, appleIdCredential.AuthorizationCode.Length);
+                    Debug.Log($"Login success: {userId} | {email} | {fullName} | {Token}");
+                    // And now you have all the information to create/login a user in your system
+                }
+            },
+            error =>
+            {
+                // Something went wrong
+                var authorizationErrorCode = error.GetAuthorizationErrorCode();
+                Debug.LogError(error.GetAuthorizationErrorCode());
+                Debug.LogError(error.LocalizedDescription);
+                Debug.LogError(error.LocalizedFailureReason);
+                Debug.LogError(error.LocalizedRecoveryOptions);
+                Debug.LogError(error.LocalizedRecoverySuggestion);
+            });
+
+#endif
     }
 
     public void SocialLogin()
     {
-        Social.localUser.Authenticate(success => {
+        Social.localUser.Authenticate(success =>
+        {
             if (success)
             {
                 Debug.Log("Authentication successful");
@@ -55,6 +131,7 @@ public class GooglePlayGameControl : MonoBehaviour
 
     public void Login()
     {
+#if UNITY_ANDROID
         PlayGamesPlatform.Instance.Authenticate((success) =>
         {
             if (success == SignInStatus.Success)
@@ -74,22 +151,9 @@ public class GooglePlayGameControl : MonoBehaviour
                 Debug.Log("Login Unsuccessful");
             }
         });
+#endif
     }
 
-    internal void ProcessAuthentication(SignInStatus status)
-    {
-        Debug.Log("status: " + status);
-        if (status == SignInStatus.Success)
-        {
-            // Continue with Play Games Services
-        }
-        else
-        {
-            // Disable your integration with Play Games Services or show a login button
-            // to ask users to sign-in. Clicking it should call
-            PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
-        }
-    }
 
     public void ShowToken()
     {
